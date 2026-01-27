b46498f3701c5f850e4ea2fead543cb56b0df676
RizzZzz09 <136895934+RizzZzz09@users.noreply.github.com>
2026-01-27 15:39:58 +0300

    chore: add AuthX JWT configuration and env settings

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,8 @@
+from datetime import timedelta
 from pathlib import Path
+from typing import List, Literal
 
+from authx import AuthXConfig
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -11,6 +14,17 @@ class Settings(BaseSettings):
 
     database_url: str = Field(default=...)
 
+    # JWT
+    JWT_SECRET_KEY: str = Field(default=...)
+    JWT_ALGORITHM: Literal["HS256", "HS384", "HS512", "RS256", "RS384", "RS512"] = Field(
+        default=...
+    )
+    JWT_TOKEN_LOCATION: List[Literal["headers", "cookies", "json", "query"]] = Field(default=...)
+    JWT_ACCESS_TOKEN_EXPIRES: int = Field(default=...)
+    JWT_REFRESH_TOKEN_EXPIRES: int = Field(default=...)
+    JWT_REFRESH_COOKIE_NAME: str = Field(default=...)
+    JWT_COOKIE_SECURE: bool = Field(default=...)
+
     model_config = SettingsConfigDict(
         env_file=ENV_PATH,
         env_file_encoding="utf-8",
@@ -20,3 +34,13 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+auth_config = AuthXConfig(
+    JWT_SECRET_KEY=settings.JWT_SECRET_KEY,
+    JWT_ALGORITHM=settings.JWT_ALGORITHM,
+    JWT_TOKEN_LOCATION=settings.JWT_TOKEN_LOCATION,
+    JWT_ACCESS_TOKEN_EXPIRES=timedelta(minutes=settings.JWT_ACCESS_TOKEN_EXPIRES),
+    JWT_REFRESH_TOKEN_EXPIRES=timedelta(days=settings.JWT_REFRESH_TOKEN_EXPIRES),
+    JWT_REFRESH_COOKIE_NAME=settings.JWT_REFRESH_COOKIE_NAME,
+    JWT_COOKIE_SECURE=settings.JWT_COOKIE_SECURE,
+)
