9f67da0f5fadb79eb2c516b1da2aeaf76f462895
RizzZzz09 <136895934+RizzZzz09@users.noreply.github.com>
2026-01-27 22:31:45 +0300

    feat: add login request and response schemas

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -10,13 +10,21 @@ class RegisterRequest(BaseModel):
     model_config = ConfigDict(extra="forbid", str_strip_whitespace=True)
 
     email: EmailStr = Field(
-        max_length=255, description="Электронная почта", examples=["abcd@gmail.com"]
+        max_length=255,
+        description="Электронная почта",
+        examples=["abcd@gmail.com"],
     )
     username: str = Field(
-        min_length=3, max_length=64, description="Имя пользователя", examples=["my_username2001"]
+        min_length=3,
+        max_length=64,
+        description="Имя пользователя",
+        examples=["my_username2001"],
     )
     password: str = Field(
-        min_length=8, max_length=128, description="Пароль", examples=["my_password123"]
+        min_length=8,
+        max_length=128,
+        description="Пароль",
+        examples=["my_password123"],
     )
 
 
@@ -29,3 +37,29 @@ class RegisterResponse(BaseModel):
     email: EmailStr
     username: str
     created_at: datetime
+
+
+class LoginRequest(BaseModel):
+    """Смеха авторизации пользователя."""
+
+    model_config = ConfigDict(extra="forbid", str_strip_whitespace=True)
+
+    login: str = Field(
+        min_length=3,
+        max_length=255,
+        description="Email или username пользователя",
+    )
+    password: str = Field(
+        min_length=8,
+        max_length=128,
+        description="Пароль пользователя.",
+    )
+
+
+class LoginResponse(BaseModel):
+    """Схема ответа после успешной авторизации пользователя."""
+
+    model_config = ConfigDict(from_attributes=True)
+
+    access_token: str
+    token_type: str = "bearer"
